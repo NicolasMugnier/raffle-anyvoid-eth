cb55c0914bd9ced4064a01783ed945a04744fd40
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-04-05 17:46:26 +0200

    feat(share): update sharing button

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,18 @@
-import { JSX } from "react"
-import './Header.css'
+import { JSX } from "react";
+import './Header.css';
+import FrameSDK from '@farcaster/frame-sdk';
+
+const shareFrame = (): void => {
+    const url: string = 'https://warpcast.com/~/compose?text=Raffle%20Frame%20by%20%40anyvoid.eth&embeds[]=https://raffle.anyvoid.xyz';
+    FrameSDK.actions.openUrl(url);
+}
 
 function Header(): JSX.Element {
     return (
         <div className="header">
-            <a href="https://warpcast.com/~/compose?text=Raffle%20Frame%20by%20%40anyvoid.eth&embeds[]=https://raffle.anyvoid.xyz">Share Frame</a>
+            <button className="share-button" onClick={shareFrame}>Share Frame</button>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
