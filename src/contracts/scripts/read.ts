a02352ca5266317bf156d954cf8bcc287a2eea3c
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-04-09 21:23:51 +0200

    chore(debug): update contract test address

--- src/contracts/scripts/read.ts
+++ src/contracts/scripts/read.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    const contractAddress: string = '0x0165878A594ca255338adfa4d48449f69242Eb8F';
+    const contractAddress: string = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
     const raffles = await ethers.getContractAt("RaffleStorage", contractAddress);
     const length: bigint = await raffles.getRafflesCount();
     console.log("Raffles Count:", length);
